eaa69a97324696518b423cf199a72675d654f3a7
trdp30 <trdp30@gmail.com>
2019-12-17 09:38:36 +0530

    custom view cart item added

--- src/components/cartItemList.js
+++ src/components/cartItemList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import { Button, Icon, List, ListItem, Text } from '@ui-kitten/components';
+import { StyleSheet, View } from 'react-native';
+import { Button, Icon, List, ListItem, Text, Card } from '@ui-kitten/components';
 import ModifyButton from './itemModifyButton';
 import AddToCartButton from './addToCartButton';
 import _ from 'lodash';
@@ -16,16 +16,36 @@ const ItemsList = (props) => {
   }
 
   const renderItem = ({ item, index }) => {
-    let rightAction = accessoryModifyButton
+    let rightAction = accessoryModifyButton({}, index)
+    if(item) {
+      return (
+        <View style={{flex: 1, flexDirection: 'row', paddingLeft: 10, paddingRight: 10, paddingTop: 20, paddingBottom: 20, borderBottomColor: 'grey', borderBottomWidth: 0.5}}>
+          <View style={{flex: 2}}></View>
+          <View style={{flex: 7}}>
+            <View>
+              <Text>{item.item.name}</Text>
+            </View>
+            <View>
+              <Text>{item.item.price} x {item.quantity} = {item.item.price * item.quantity}</Text>
+            </View>
+          </View>
+          <View style={{flex: 3}}>
+            {rightAction}
+          </View>
+        </View>
+      )
+    } else {
+      return (<View></View>)
+    }
 
-    return (
-      <ListItem
-        title={item.item.name}
-        description={item.item.price}
-        titleStyle={{textTransform: "capitalize", fontSize: 16}}
-        accessory={rightAction}
-      />
-    );
+    // return (
+    //   <ListItem
+    //     title={item.item.name}
+    //     description={item.item.price}
+    //     titleStyle={{textTransform: "capitalize", fontSize: 16}}
+    //     accessory={rightAction}
+    //   />
+    // );
   }
 
   return (
