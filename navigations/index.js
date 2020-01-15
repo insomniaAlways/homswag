41641979951d51c0e6359e3efe460c922f9add8c
trdp30 <trdp30@gmail.com>
2020-01-15 23:00:33 +0530

    hearder right component added

--- navigations/index.js
+++ navigations/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
@@ -8,17 +7,16 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import DashboardScreen from '../src/screens/DashboardScreen';
 import ItemsScreen from '../src/screens/ItemsScreen';
 import CartScreen from '../src/screens/CartScreen';
-import CartButton from '../src/components/cartButton';
 import ScheduleAppointmentScreen from '../src/screens/scheduleAppointmentScreen'
 import PaymentScreen from '../src/screens/PaymentScreen';
 import DefaultStyles from '../src/style/customStyles';
-import { EvilIcons, FontAwesome, Feather } from '@expo/vector-icons';
 import ProfileScreen from '../src/screens/ProfileScreen';
 import AddAddressScreen from '../src/screens/AddAddressScreen';
 import OrderHistoryScreen from '../src/screens/OrderHistoryScreen';
 import ContactScreen from '../src/screens/ContactScreen';
 import SideDrawer from '../src/components/sideDrawer';
 import AddressScreen from '../src/screens/AddressScreen';
+import HeaderRightView from '../src/components/headerRight';
 
 const AppNavigator = createStackNavigator({
     Dashboard: {
@@ -49,21 +47,7 @@ const AppNavigator = createStackNavigator({
         fontWeight: 'bold',
         flex: 1
       },
-      headerRight: () => {
-        return (
-          <View style={{paddingRight: 20, flexDirection: 'row'}}>
-            <EvilIcons name="search" size={40} color="#fff" style={{marginRight: 10}}/>
-            <CartButton navigation={navigation}/>
-          </View>
-        )
-      },
-      // headerLeft: () => {
-      //   return (
-      //     <View style={{paddingLeft: 10}}>
-      //       <Feather name="menu" size={30} color="#fff" style={{marginRight: 10}}/>
-      //     </View>
-      //   )
-      // }
+      headerRight: <HeaderRightView navigation={navigation}/>
     })
   }
 );
