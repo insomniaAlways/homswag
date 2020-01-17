51ef4d7bc225c4c889bb2b61c9dcf760536d1e78
trdp30 <trdp30@gmail.com>
2020-01-17 21:12:38 +0530

    images url updated

--- src/components/offerCard.js
+++ src/components/offerCard.js
@@ -3,10 +3,10 @@ import { View, Text, Image } from 'react-native';
 
 function OfferCard({ id, style }) {
   return (
-    <View style={[{height: '100%', width: 200, marginRight: 10}, style]}>
+    <View style={[{height: '100%', width: 370, marginRight: 10}, style]}>
       <Image 
         style={{height: "100%", width: "100%", borderRadius: 10}}
-        source={{uri: `https://i.picsum.photos/id/${id}/200/200.jpg`}}
+        source={{uri: `https://i.picsum.photos/id/${id}/400/200.jpg`}}
       />
     </View>
   )
