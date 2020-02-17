56dc699e4550bb54a6c3883eff3167e34e403eec
trdp30 <trdp30@gmail.com>
2020-02-17 08:53:53 +0530

    package loading state updated

--- src/components/offerView.js
+++ src/components/offerView.js
@@ -9,7 +9,9 @@ function OfferView(props) {
   return (
     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
       {packages.isLoading ? 
-        <View><Text>Loading..</Text></View> :
+        <View style={{height: '100%', width: 380, marginRight: 10, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{fontFamily: 'roboto-light-italic'}}>Loading...</Text>
+        </View> :
         packages.values.map((packageService, index) => <OfferCard navigation={navigation} key={packageService.id} packageService={packageService} styles={index == 0 && { marginLeft: 10 }}/>)
       }
     </ScrollView>
